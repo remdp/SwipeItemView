192d1adaa98a4d8161845cccd3b8dd3ca096e0b4
remdp <remmdp@GMAIL.COM>
2016-12-14 20:45:46 +0200

    add scroll

--- app/src/main/java/com/example/java/swipeitemview/MainActivity.java
+++ app/src/main/java/com/example/java/swipeitemview/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.java.swipeitemview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import View.SwipeItemView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeItemView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = (SwipeItemView) findViewById(R.id.customView);
     }
 
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        view.dispatchTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 }
--- library/src/main/java/View/SwipeItemView.java
+++ library/src/main/java/View/SwipeItemView.java
@@ -1,24 +1,60 @@
 package View;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
+import android.text.Layout;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 /**
  * Created by java on 14.12.2016.
  */
 
-public class SwipeItemView  extends FrameLayout {
+public class SwipeItemView  extends FrameLayout implements GestureDetector.OnGestureListener {
+
+    private GestureDetectorCompat mGestureDetectorCompat;
+    private View mTopView;
+    private View mBottomView;
+
+    private boolean isLeftDirection;
 
     public SwipeItemView(Context context) {
         super(context);
+        mGestureDetectorCompat = new GestureDetectorCompat(getContext(), this);
     }
 
     public SwipeItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mTopView = new View(getContext());
+        mBottomView = new View(getContext());
+
+        LayoutParams params = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+
+        mTopView.setLayoutParams(params);
+        mBottomView.setLayoutParams(params);
+
+        ViewCompat.setBackground(mTopView, new ColorDrawable(Color.RED));
+        ViewCompat.setBackground(mBottomView, new ColorDrawable(Color.GREEN));
+
+        addView(mBottomView);
+        addView(mTopView);
+
+        mGestureDetectorCompat = new GestureDetectorCompat(getContext(), this);
     }
 
     public SwipeItemView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -35,27 +71,56 @@ public class SwipeItemView  extends FrameLayout {
         return super.onInterceptTouchEvent(ev);
     }
 
+
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-                break;
-            }
-
-            case MotionEvent.ACTION_MOVE: {
-                break;
-            }
-
-            case MotionEvent.ACTION_UP: {
-                break;
-            }
+        mGestureDetectorCompat.onTouchEvent(ev);
+        if(ev.getAction() == MotionEvent.ACTION_UP){
+            //if(mTopView.getX()<mTopView.getMeasuredWidth()/2;
         }
+
         return super.onTouchEvent(ev);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
+    }
+
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        Log.d(SwipeItemView.class.getSimpleName(), "scroll");
+       //if(motionEvent1.getX()<mTopView.getMeasuredWidth()/2){
+            mTopView.setX(motionEvent1.getX());
+      //  }
+
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 }
